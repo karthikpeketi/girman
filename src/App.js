f1791c81462c8a93d1b953f736829bcedb02827b
karthikpeketi <karthikreddypeketi@gmail.com>
2024-11-30 03:13:41 -0800

    add the /girman at the end of the url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import SearchResults from "./pages/SearchResults";
 import Header from "./pages/Header";
 import { SearchProvider } from "./context/SearchContext";
+
 const App = () => {
 	return (
 		<SearchProvider>
@@ -12,6 +13,9 @@ const App = () => {
 					<Header />
 					<div className="flex flex-col items-center w-full h-[90%] overflow-y-auto bg-gradient">
 						<Routes>
+							{/* Redirect from root `/` to `/girman` */}
+							<Route path="/" element={<Navigate to="/girman" replace />} />
+							{/* Route definitions */}
 							<Route path="/girman" element={<SearchPage />} />
 							<Route path="/search" element={<SearchResults />} />
 						</Routes>
