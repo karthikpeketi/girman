cd8d22f7c74063d983f85cbf7bf864d2d74f71df
karthikpeketi <karthikreddypeketi@gmail.com>
2024-12-19 00:07:43 +0530

    Add reusable Card and DialogBox components, update searchContext with filterUsers state, and integrate user data JSON

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -4,9 +4,10 @@ export const SearchContext = createContext();
 
 export const SearchProvider = ({ children }) => {
 	const [searchTerm, setSearchTerm] = useState("");
+	const [filteredUsers, setFilteredUsers] = useState([]);
 
 	return (
-		<SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+		<SearchContext.Provider value={{ searchTerm, setSearchTerm, filteredUsers, setFilteredUsers }}>
 			{children}
 		</SearchContext.Provider>
 	);
